3b99821521e145c359e76c6e9e2c85e957e23c68
FKouhai <frandres00@gmail.com>
2026-03-07 13:12:56 +0100

    chore: bump helium to v0.9.4.1

--- flake.nix
+++ flake.nix
@@ -32,11 +32,11 @@
           pkgs.appimageTools.wrapType2 rec {
 
             pname = "helium";
-            version = "0.9.3.1";
+            version = "0.9.4.1";
 
             src = pkgs.fetchurl {
               url = "https://github.com/imputnet/helium-linux/releases/download/${version}/${pname}-${version}-x86_64.AppImage";
-              sha256 = "sha256-wUmFmfZPWSvPzArbegegQpY1CFu/XAguqPQpINDE2qY=";
+              sha256 = "sha256-N5gdWuxOrIudJx/4nYo4/SKSxakpTFvL4zzByv6Cnug=";
             };
 
             _enableFeatures =
